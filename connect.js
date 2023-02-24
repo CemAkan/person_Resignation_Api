a85fd9ac82196a88d45e4aeaaa7428c4df97bfa1
CemAkan <cemakan5014@gmail.com>
2023-02-24 13:29:25 +0300

    comment lines added

--- connect.js
+++ connect.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
 
+//--> Define dotenv variables <--
 var NAME = process.env.NAME;
 var DB = process.env.DB;
 var DIALECT = process.env.DIALECT;
 var HOST = process.env.HOST;
 var PASSWORD = process.env.PASSWORD;
 
+//--> Database connection <--
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(DB, NAME, PASSWORD, {
   host: HOST,
   dialect: DIALECT,
 });
 
+//--> Define and export dataBase variable <--
 var dataBase = {};
 
 dataBase.Person = sequelize.import(__dirname + "/model/person.js");
